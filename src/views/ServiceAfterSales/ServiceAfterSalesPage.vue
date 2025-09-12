7d918f9b1b227418dcd96fbd37fc4de339e87ac2
lihaoyang <l15639757696@163.com>
2025-09-12 10:26:28 +0800

    售后申请渠道添加 物流配送 类型

--- src/views/ServiceAfterSales/ServiceAfterSalesPage.vue
+++ src/views/ServiceAfterSales/ServiceAfterSalesPage.vue
@@ -260,6 +260,7 @@ export default {
         { name: '网页端', ID: 0 },
         { name: '移动端', ID: 1 },
         { name: '人工客服', ID: 2 },
+        { name: '物流配送', ID: 3 },
       ],
       dateList: [
         { label: '近七天', value: 'last7Date' },
